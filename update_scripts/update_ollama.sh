1194c98ae75ed7da8b894e54716f04166cf1fa1b
Juha Leivo <juha@leivo.org>
2026-03-17 09:30:18 +0200

    Feat: Created GPU shared config file

--- update_scripts/update_ollama.sh
+++ update_scripts/update_ollama.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
+#
+# Author: Juha Leivo
+# Version: 1.0.0
+# Date: 2026-03-17
+#
+# History
+#   1.0.0 - 2026-03-17,     Added official header, changed ollama GPU config to 
+#                           use variable and centralized GPU config file
+#
+
+source ./GPU_config
+
+GPU_CONFIG="--gpus=all -e CUDA_VISIBLE_DEVICES=$GPU0,$GPU2,$GPU3,$GPU4"
+OLLAMA_VERSION="0.17.4"
 
 function stop_container() {
-    docker stop "$1" > /dev/null  || { echo "Stopping $1 failed?!"; exit 1; }
+    docker stop "$1" > /dev/null  || { echo "Stopping $1 failed?!"; }
 }
 
 function remove_container() {
     docker container rm -f "$1" > /dev/null || { echo "Clean up failed for $1!"; exit 1; }
-
 }
 
 function update_container() {
@@ -20,7 +33,18 @@ function cleanup() {
 
 function ollama_start() {
     echo -n "  Starting ollama "
-    docker run -d --gpus=all -e CUDA_VISIBLE_DEVICES=GPU-0a895c3d-0f7f-46f1-50ab-1f84e96f7083,GPU-16bf6b6f-a008-eecc-50c8-eac39eae9f7d,GPU-016209dd-9c62-0a1b-0fde-97cfb3b0c090 -v ollama:/root/.ollama -p 11434:11434 -e OLLAMA_MAX_LOADED_MODELS=6 -e OLLAMA_FLASH_ATTENTION=1 --name ollama --restart always ollama/ollama && { echo "Updated ollama container successfully!"; return 0; }
+    docker run -d "$GPU_CONFIG" \
+        -v ollama:/root/.ollama \
+        -v ollama-import:/import \
+        -v /srv/ollama/container_modelfiles:/modelfiles \
+        -p 11434:11434 \
+        -e OLLAMA_MAX_LOADED_MODELS=4 \
+        -e OLLAMA_NUM_PARALLEL=4 \
+        -e OLLAMA_FLASH_ATTENTION=1 \
+        -e OLLAMA_KV_CACHE_TYPE=q8_0 \
+        -e OLLAMA_KEEP_ALIVE=24h \
+        --name ollama \
+        --restart always ollama/ollama:$OLLAMA_VERSION && { echo "Updated ollama container successfully!"; return 0; }
     return 1
 }
 
@@ -54,9 +78,9 @@ if ! ollama_start; then
     echo "Starting updated ollama failed..."
 fi
 
-if ! open-webui_start; then
-    echo "Starting Open webUI failed..."
-fi
+#if ! open-webui_start; then
+#    echo "Starting Open webUI failed..."
+#fi
 
 
 cleanup
